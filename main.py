73aa8135ed1d285c1cc95300910a67afc9c6032b
Donny van Eijk <donnyvaneijk873@gmail.com>
2023-11-06 14:00:02 +0100

    the stuff

--- main.py
+++ main.py
@@ -30,8 +30,9 @@ def get_current_location():
         return None, None
 
 def get_weather(latitude, longitude):
-    api_key = os.getenv('APIKEYWEATHER')
-    api_url = f'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
+    api_key = os.getenv('WEATHER_API_KEY')
+    api_url_base = os.getenv('WEATHER_API_URL', 'http://dataservice.accuweather.com')
+    api_url = f'{api_url_base}/locations/v1/cities/geoposition/search'
 
     params = {
         'apikey': api_key,
@@ -39,50 +40,24 @@ def get_weather(latitude, longitude):
     }
 
     response = requests.get(api_url, params=params)
+    print(api_url)
 
     if response.status_code == 200:
         location_data = response.json()
         location_key = location_data['Key']
 
-        api_url = f'http://dataservice.accuweather.com/currentconditions/v1/{location_key}'
+        api_url = f'{api_url_base}/currentconditions/v1/{location_key}'
 
         params = {
             'apikey': api_key,
         }
 
-        response = requests.get(api_url, params=params)
+        print(api_url)
+        return requests.get(api_url, params=params)
 
-        if response.status_code == 200:
-            weather_data = response.json()[0]
 
-            temperature_fahrenheit = weather_data.get('Temperature', {}).get('Imperial', {}).get('Value')
 
-            temperature_celsius = (temperature_fahrenheit - 32) * 5 / 9
 
-            icon = weather_data.get('WeatherIcon')
-            if temperature_celsius >= 10:
-                is_bikeable = True
-            else:
-                is_bikeable = False
-
-            saved_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-            response_data = {
-                "temperature": temperature_celsius,
-                "icon": icon,
-                "bikeable": is_bikeable,
-                "location": {
-                    "lat": latitude,
-                    "lon": longitude
-                },
-                "saved_at": saved_at
-            }
-
-            return response_data
-        else:
-            return {'error': 'Unable to fetch weather data'}
-    else:
-        return {'error': 'Location not found'}
 
 @app.route('/')
 def weather():
@@ -95,4 +70,4 @@ def weather():
         return jsonify({'error': 'Unable to retrieve current location coordinates'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
